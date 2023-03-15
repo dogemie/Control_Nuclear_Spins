eeeaa046ce52c85f5bbe1339b2de7c1d7d54d3b2
zorocrit <dogyeom74@gmail.com>
2023-03-15 09:43:09 +0900

    data 03.15

--- Init13C_Sec/MakeDatasetFinal_for_test.py
+++ Init13C_Sec/MakeDatasetFinal_for_test.py
@@ -24,7 +24,8 @@ from tqdm import tqdm
 from tqdm import trange
 from scipy.linalg import fractional_matrix_power
 
-totalstart = time.time()
+totalstart = time.time() #시작 시간 저장
+
 #Generating gate function
 def UO(B1,B2,a,D1,D2):
     i   = 1j
@@ -66,16 +67,6 @@ Sweep = 1001
 N = Sweep
 B = 403 #[G] magnetic field
 
-# 13C nuclear spin parameters
-# gammaN = 2*pi*1.071e-3 #[MHz/G]
-# Al    = 2*pi*0.3334155111 #[MHz] # A_|| hyperfine term
-# Ap = 2*pi*1.7777771 #[MHz] # A_per hyperfine term
-
-#al 0.05~0.8
-#ap 0.05~0.2, 0.3
-
-
-
 T = 5; # sweep tau [us]
 t = np.linspace(0,T,N)
 n = 32; # number of pi pulses
@@ -97,25 +88,25 @@ U180xmm = UO(0,1,pi/2,0,0)
 
 # Define initial state of the system
 
-irho_p = np.array([[1,0,0],[0,0,0],[0,0,0]]) #;0,0,0;0,0,0]
+irho_p = np.array([[1,0,0],[0,0,0],[0,0,0]]) #[0,0,0;0,0,0]
 
-irho_m = np.array([[0,0,0],[0,0,0],[0,0,1]]) #0,0,0;0,0,1]
+irho_m = np.array([[0,0,0],[0,0,0],[0,0,1]]) #[0,0,0;0,0,1]
 
-irho_z = np.array([[0,0,0],[0,1,0],[0,0,0]]) #0,1,0;0,0,0]
+irho_z = np.array([[0,0,0],[0,1,0],[0,0,0]]) #[0,1,0;0,0,0]
 
-irho_mix = np.array([[1/2,0,0],[0,1/2,0],[0,0,0]])
+irho_mix = np.array([[1/2,0,0],[0,1/2,0],[0,0,0]]) #[1/2,0,0;0,1/2,0;0,0,0]
 
 irho_Z = np.array([[0,0,0],[0,0,0],[0,0,1]]) #target state
 
 irho_MIX = np.array([[1/2,0,0],[0,0,0],[0,0,1/2]])
 
-irho = np.kron(irho_z,irho_MIX)
-trace = [1, 1, -1, 100]
+irho = np.kron(irho_z,irho_MIX) #initial state
+trace = [1, 1, -1, 100] # trace of the X, Y, Z, and total density matrices
 
 
-co = 1
 
-def state_fidelity(rho_1, rho_2):
+
+def state_fidelity(rho_1, rho_2): #fidelity
         if np.shape(rho_1) != np.shape(rho_2):
             print("Dimensions of two states do not match.")
             return 0
@@ -124,7 +115,7 @@ def state_fidelity(rho_1, rho_2):
             fidelity = np.trace(fractional_matrix_power(sqrt_rho_1 @ rho_2 @ sqrt_rho_1, 1 / 2)) ** 2
             return np.real(fidelity)
 
-def problem(vari):
+def problem(vari): 
         #for e Ry(pi/2)
         rho1 = np.kron(U090yp,I)@irho@(np.kron(U090yp,I).conj().T)                              # Ry 90도
 
@@ -164,16 +155,6 @@ def problem(vari):
         yy = (np.trace(Iy@partial_trace(rho8,1))).real
         zz = (np.trace(Iz@partial_trace(rho8,1))).real
         
-        
-        # print("rho1 :", partial_trace(rho1,1))
-        # print("rho8 :", partial_trace(rho8,1))
-        # print(xx,yy,zz)
-        # ax.plot(xx,yy,zz,'ro')
-        # plt.pause(0.001)
-        # cost = ((np.abs(0-xx))**2+(np.abs(0-yy))**2+(np.abs(1-zz))**2)**(1/2)
-        
-        # cost = np.abs(0 + xx) + np.abs(0 + yy) + np.abs(1 - zz)
-        
         cost = state_fidelity(irho_Z, partial_trace(rho8, 1))
         
         if(np.abs(xx) < np.abs(trace[0])):
@@ -182,10 +163,6 @@ def problem(vari):
             trace[1] = yy
         if(zz > trace[2]):
             trace[2] = zz
-        # if(cost < trace[3]):
-        #     trace[3] = cost
-            # print(partial_trace(rho8,1))
-        
         return cost
         
 aa = []
@@ -194,10 +171,8 @@ count = 1
 tot_sum = 0
 
 
-for ccc in tqdm(range(5)):
+for ccc in tqdm(range(5)): # range 번의 실험을 진행한다.
     trace = [1, 1, -1, 100]
-    # fig = plt.figure(figsize=(9, 6))
-    # ax = fig.add_subplot(111, projection='3d')
     start = time.time()
     #for making 13C nuclear random dataset
     gammaN = 2*pi*1.071e-3 #[MHz/G]
@@ -209,11 +184,11 @@ for ccc in tqdm(range(5)):
 
     Sa= []
 
-    for h in range(N):
-        ham = Al*np.kron(sz,Iz) + Ap*np.kron(sz,Ix) + B*gammaN*np.kron(I,Iz)
+    for h in range(N): # N개의 pulse를 생성한다.
+        ham = Al*np.kron(sz,Iz) + Ap*np.kron(sz,Ix) + B*gammaN*np.kron(I,Iz) # Hamiltonian
         
         eigvals = np.linalg.eigh(ham)[0]            # diagonalizing the Hamiltonian 여기서부터 문제 
-        eigvecs = -1*np.linalg.eigh(ham)[1]
+        eigvecs = -1*np.linalg.eigh(ham)[1]         # eigenvectors
         E = np.diag(eigvals)                        # exponent of eigenvalues
         U_H= eigvecs.conj().T                       # unitary matrix formed by eigenvectors
 
@@ -222,28 +197,22 @@ for ccc in tqdm(range(5)):
         U_e2 = (U_H.conj().T)@(linalg.expm(-i*E*t[h]/2)@U_H) # for tau/2
         U_e  = (U_H.conj().T)@(linalg.expm(-i*E*t[h])@U_H)  # for tau
         rho_1 = U_e2 @ rho_0 @ (U_e2.conj().T)                  # first tau/2
-        for k in range(n-1):
-            rho_1 = U_e @ np.kron(U180xp,I) @ rho_1 @ (np.kron(U180xp,I).conj().T) @ (U_e.conj().T)
+        for k in range(n-1):                                   # N과 tau를 N개 생성
+            rho_1 = U_e @ np.kron(U180xp,I) @ rho_1 @ (np.kron(U180xp,I).conj().T) @ (U_e.conj().T) # N & tau
             
-        rho_2 = U_e2 @ np.kron(U180xp,I) @ rho_1 @ (np.kron(U180xp,I).conj().T) @ (U_e2.conj().T)
+        rho_2 = U_e2 @ np.kron(U180xp,I) @ rho_1 @ (np.kron(U180xp,I).conj().T) @ (U_e2.conj().T) # last N & tau/2
         rho_3 = np.kron(U090xmp,I) @ rho_2 @ ((np.kron(U090xmp,I)).conj().T)    # last pi/2
         res1 = (np.trace(irho_z@partial_trace(rho_3,2))).real                   # NV state 0 population readout
-        Sa.append(res1)
+        Sa.append(res1)                                                       # append to list
         
-    #plot graph
-    # plt.subplot(2,1,1)
-    # plt.plot(t,Sa)
-    index = Sa.index(min(Sa))
-    tau=t[index]
-    # print(Al, Ap, tau)
-    # print(trace)
-    # print("Al = ", Al, "Ap = ", Ap, "tau = ", tau)
+    index = Sa.index(min(Sa)) # list에서 가장 작은 값을 가지는 index를 찾는다.
+    tau=t[index] # 그 index에 해당하는 tau를 찾는다.
 
-    ham = Al*np.kron(sz,Iz) + Ap*np.kron(sz,Ix) + B*gammaN*np.kron(I,Iz)
+    ham = Al*np.kron(sz,Iz) + Ap*np.kron(sz,Ix) + B*gammaN*np.kron(I,Iz) # Hamiltonian
     eigvals = np.linalg.eigh(ham)[0] # diagonalizing the Hamiltonian 
-    eigvecs = -1*np.linalg.eigh(ham)[1]
+    eigvecs = -1*np.linalg.eigh(ham)[1] # eigenvectors
     E = np.diag(eigvals)             # exponent of eigenvalues
-    U_H= eigvecs.conj().T  
+    U_H= eigvecs.conj().T         # unitary matrix formed by eigenvectors
 
     xx=0
     yy=0
@@ -254,29 +223,23 @@ for ccc in tqdm(range(5)):
 
     tol = 1e-8 #tolerance
 
-    for p in range(1):
+    for p in range(1): # 1번의 실험을 진행한다.(지역 최적화 알고리즘을 사용할 경우에 수정한다.)
         vari=[tau,9,0.1*tau, 9]  #초기값
-        # vari = [0.5,15,0.3]
         bounds = [(0.85*tau,1.15*tau),(1.0,50.0),(0.5*tau,0.8*tau),(1.0,50.0)] #boundary
         
         res4 = optimize.shgo(problem,bounds=bounds,iters=4,options={'xtol':1e-15,'ftol':1e-15}) #SHGO method
-        # res4 = optimize.minimize(problem,vari,method='Nelder-Mead',options={'xtol':tol,'ftol':tol}) #Nelder-Mead method
-        res4['x'][1] = round(res4['x'][1])
-        res4['x'][3] = round(res4['x'][3])
-        # print(res4)
-        if(np.abs(res4['fun']) < 0.05):
+        res4['x'][1] = round(res4['x'][1]) #rounding
+        res4['x'][3] = round(res4['x'][3]) #rounding
+
+        if(np.abs(res4['fun']) < 0.05): #fidelity가 0.05보다 작으면 성공
             dd.append([Al, Ap, res4['x'][0], res4['x'][1], res4['x'][2], res4['x'][3], res4['fun'], trace[0], trace[1], trace[2], res4['nfev'], "shgo1"])
             end = time.time()
             final = end - start
-            # print(final)
-            # print("count : ", count)
             count = count + 1
-        else:
+        else: #fidelity가 0.05보다 크면 실패
             aa.append([Al, Ap, res4['x'][0], res4['x'][1], res4['x'][2], res4['x'][3], res4['fun'], trace[0], trace[1], trace[2], res4['nfev'], "fail"])
             end = time.time()
             final = end - start
-            # print(final)
-            # print("count : ", count)
             count = count + 1
 stdout.write("\n")
 
@@ -286,10 +249,10 @@ printdate = date.strftime('%Y%m%d_%H%M%S')
 print(date)
 
 # 결과들을 list에 저장하여 csv파일로 저장
-df4 = pd.DataFrame(dd)
+df4 = pd.DataFrame(dd) 
 df4.rename(columns={0:"Al", 1:"Ap", 2: "Xtau", 3: "XN", 4: "Ztau", 5: "ZN", 6: "fun", 7: "xx", 8: "yy", 9: "zz", 10: "fev"}, inplace=True)
 df4.to_csv('C:/Users/Administrator/Dogyeom(2023.01.01)/KIST_intern/Task1/Control_Nuclear_Spins/Init13C_Sec/for_test/ppd' + printdate + '.csv',index=False)
-df2 = pd.DataFrame(aa)
+df2 = pd.DataFrame(aa) 
 df2.rename(columns={0:"Al", 1:"Ap", 2: "Xtau", 3: "XN", 4: "Ztau", 5: "ZN", 6: "fun", 7: "xx", 8: "yy", 9: "zz", 10: "fev"}, inplace=True)
 df2.to_csv('C:/Users/Administrator/Dogyeom(2023.01.01)/KIST_intern/Task1/Control_Nuclear_Spins/Init13C_Sec/for_test/ffd' + printdate + '.csv',index=False)
 
@@ -298,11 +261,4 @@ print('success')
 totalend = time.time()
 print(totalend - totalstart)
 
-# df3 = pd.read_csv('C:/Users/Administrator/Dogyeom(2023.01.01)/KIST_intern/Task1/Control_Nuclear_Spins/initialize13Cspin/testdata_second.csv')
-
-# df1 = pd.concat([df3, df4], axis=0)
-# df1.to_csv('C:/Users/Administrator/Dogyeom(2023.01.01)/KIST_intern/Task1/Control_Nuclear_Spins/initialize13Cspin/testdata_second' + printdate + '.csv', index=False)
-# # %%
-# print(U090xp)
-# print(U090yp)
 # # %%
